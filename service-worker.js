af74232c95c170bc263cc13249f8ea3d7986299a
SEMİH İŞBAŞ <sisbas@yahoo.com>
2025-07-21 12:02:01 +0300

    V2

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("sut-takip-cache").then(function (cache) {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
+
+const CACHE_NAME = "sut-takip-cache-v1";
+const urlsToCache = [
+  "sut_rapor_panel.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
